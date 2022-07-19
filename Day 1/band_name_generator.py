32ecfe55185805329999c688f82c22a52c0848c5
MDaniele Fofana <daniele.fofana@gmail.com>
2022-07-19 17:14:12 +0000

    Edited code in generator

--- Day 1/band_name_generator.py
+++ Day 1/band_name_generator.py
@@ -6,7 +6,7 @@ city = input ("What is the name of the city you grew up in?\n")
 #3. Ask the user for the name of a pet.
 pet_name = input("What is the name of your pet?\n")
 #4. Combine the name of their city and pet and show them their band name.
-band_name = city + "" + pet_name
+band_name = city + " " + pet_name
 print("Your band name is " + band_name)
 #5. Make sure the input cursor shows on a new line, see the example at:
 #   https://replit.com/@appbrewery/band-name-generator-end
